008af56265fb7b3e004f6795391c4d8c61841aef
Coco Jiang <cocojiang505@gmail.com>
2021-11-19 23:57:52 -0500

    fixed errors in UserInfo

--- src/User/UserInfo.java
+++ src/User/UserInfo.java
@@ -1,93 +1,70 @@
 package User;
 
 import Graph.DirectedGraph;
-import Posts.Post;
 import Resource.Resource;
 import constants.Achievements;
 
-import java.beans.PropertyChangeSupport;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The class that stores all information of a user.
+ */
 public class UserInfo {
-    private String userName;
+    private String username;
     private String email;
     private String password;
     private int rewardPoints;
     private LocalDate lastLogin;
     private int totalLogins;
-
-    public ArrayList<Post> getListOfPost() {
-        return listOfPost;
-    }
-
-    private ArrayList<Post> listOfPost = new ArrayList<>();
-    private ArrayList<DirectedGraph> listOfGraph = new ArrayList<>();
+    private ArrayList<String> listOfPostId = new ArrayList<>();
+    private Map<String, DirectedGraph> mapOfGraph = new HashMap<>();
     private ArrayList<Resource> listOfResource = new ArrayList<>();
-
-    public Map<String, Boolean> getMapOfAchievement() {
-        return mapOfAchievement;
-    }
-
     private Map<String, Boolean> mapOfAchievement = new HashMap<>();
 
-    private final PropertyChangeSupport observable = new PropertyChangeSupport(this);
-
-
-    /**
-     * Instantiates UserInfo with userName, email, and password of user.
-     * @param userName the user's username.
-     * @param email the user's email.
-     * @param password the user's password.
-     */
-    public UserInfo(String userName, String email, String password) {
-        this.userName = userName;
+    public UserInfo(String username, String email, String password) {
+        this.username = username;
         this.email = email;
         this.password = password;
         this.rewardPoints = 0;
         this.lastLogin = LocalDate.now();
         this.totalLogins = 0;
+        initializeMapOfAchievement();
     }
 
+
     /**
-     * Adds a Post to the user's listOfPosts.
-     * @param post the post that the user created.
+     * add a post id.
+     * @param id the id of the post being added.
      */
-    public void addPost(Post post){
-        listOfPost.add(post);
-        boolean trigger = RequestAchievement(Achievements.ARRAY_OF_POST_THRESHOLDS, Achievements.MAP_POST_THRESHOLDS_TO_ACHIEVEMENT, listOfPost.size());
-        if (trigger){
-            this.setRewardPoints(Achievements.MAP_POST_THRESHOLDS_TO_REWARD.get(listOfPost.size()));
-        } //put it in facade when we write post()
+    public void addToListOfPostId(String id) {
+        this.listOfPostId.add(id);
     }
 
     /**
-     * Increases the totalLogins by 1.
+     * increase the total number of logins by 1.
      */
     public void incrementTotalLogins() {
         totalLogins += 1;
-        boolean trigger = RequestAchievement(Achievements.ARRAY_OF_TOTAL_LOGINS_THRESHOLDS, Achievements.MAP_TOTAL_LOGINS_THRESHOLDS_TO_ACHIEVEMENT, totalLogins);
-        if(trigger){
-            this.setRewardPoints(Achievements.MAP_TOTAL_LOGINS_THRESHOLDS_TO_REWARD.get(totalLogins));
-        }
     }
 
-
-
+    /**
+     * Initialize mapOfAchievement, with all achievement not received.
+     */
     public void initializeMapOfAchievement() {
         for (String achievement : Achievements.ARRAY_OF_ALL_ACHIEVEMENTS) {
             mapOfAchievement.put(achievement, false);
         }
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String newName) {
-        this.userName = newName;
+    public void setUsername(String newName) {
+        this.username = newName;
     }
 
     public void setEmail(String newEmail) {
@@ -106,12 +83,16 @@ public class UserInfo {
         this.rewardPoints = rewardPoints;
     }
 
-    public void addGraph(DirectedGraph graph){
-        listOfGraph.add(graph);
+    public void addRewardPoints(int rewardPoints) {
+        this.rewardPoints += rewardPoints;
     }
 
-    public ArrayList<DirectedGraph> getListOfGraph(){
-        return this.listOfGraph;
+    public void addGraph(DirectedGraph graph){
+        mapOfGraph.put(graph.getName(), graph);
+    }
+
+    public Map<String, DirectedGraph> getMapOfGraph(){
+        return this.mapOfGraph;
     }
 
     public void addResource(Resource resource){
@@ -124,19 +105,45 @@ public class UserInfo {
 
     @Override
     public String toString() {
-        return "User: " + this.userName;
+        return "UserInfo{" +
+                "userName='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", rewardPoints=" + rewardPoints +
+                ", lastLogin=" + lastLogin +
+                ", totalLogins=" + totalLogins +
+                ", listOfPostId=" + listOfPostId +
+                ", listOfGraph=" + mapOfGraph +
+                ", listOfResource=" + listOfResource +
+                ", mapOfAchievement=" + mapOfAchievement +
+                '}';
     }
 
+    public ArrayList<String> getListOfPostId() {
+        return listOfPostId;
+    }
 
+    public int getTotalLogins() {
+        return totalLogins;
+    }
 
-//    Deprecated:
+    public String getEmail() {
+        return email;
+    }
 
-//    private void checkAndRequestAchievement(int[] thresholds, int property, String achievementName) {
-//        for (int threshold : thresholds) {
-//            if (property == threshold) {
-//                observable.firePropertyChange(achievementName, property - 1, property);
-//            }
-//        }
-//    }
+    public String getPassword() {
+        return password;
+    }
 
+    public LocalDate getLastLogin() {
+        return lastLogin;
+    }
+
+    public Map<String, Boolean> getMapOfAchievement() {
+        return mapOfAchievement;
+    }
+
+    public boolean hasGraph(String graphName) {
+        return mapOfGraph.containsKey(graphName);
+    }
 }
