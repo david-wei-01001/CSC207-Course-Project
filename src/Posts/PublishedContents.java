a6a12be57fe69496d6e4fa8f9d3c660f881ba5e6
Arthursleeps16hrs <b1246248104@gmail.com>
2021-11-20 18:02:50 -0500

    Added some java doc for the posts class.

--- src/Posts/PublishedContents.java
+++ src/Posts/PublishedContents.java
@@ -10,41 +10,69 @@ public class PublishedContents implements Likable, Visible, Serializable {
     private boolean visible = true;
     private final User creator;
 
+    /**
+     * Constructor for the published contents class
+     * @param id: The id of the comment
+     * @param creator: The creator of the comment
+     */
     public PublishedContents(String id, User creator) {
         this.id = id;
         this.creator = creator;
     }
 
+    /**
+     * Get the ID of the published contents
+     * @return Then ID of the published contents
+     */
     public String getId() {
         return id;
     }
 
-
+    /**
+     * Get the creator of the published contents
+     * @return Then creator of the published contents
+     */
     public User getCreator() {
         return creator;
     }
 
-
+    /**
+     * Get the number of like of the published contents
+     * @return Then number of like of the published contents
+     */
     @Override
     public int getNumberOfLike() {
         return numLike;
     }
 
+    /**
+     * Increment the like by 1
+     * @return The incremented likes
+     */
     @Override
     public void like() {
         numLike += 1;
     }
 
+    /**
+     * Set the status of published contents to true
+     */
     @Override
     public void setVisible() {
         visible = true;
     }
 
+    /**
+     * Set the status of published contents to false
+     */
     @Override
     public void setInvisible() {
         visible = false;
     }
 
+    /**
+     * Return the status of the visibility
+     */
     @Override
     public boolean visibility() {
         return visible;
