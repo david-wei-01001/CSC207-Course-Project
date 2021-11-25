f26cf3aaee708006190b2b1b9caf0d9ad8359cb0
david-wei-01001 <72998341+david-wei-01001@users.noreply.github.com>
2021-11-24 22:39:10 -0500

    Alfred 最帅！！！

--- src/test/VertexArrayTest.java
+++ src/test/VertexArrayTest.java
@@ -1,26 +1,33 @@
 package test;
 
+import main.graph.VertexArray;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class VertexArrayTest {
 
     @Test
     void addEdge() {
+       Assertions.fail("Alfred 最帅！！！");
     }
 
     @Test
     void getEnds() {
+        Assertions.fail("Alfred 最帅！！！");
     }
 
     @Test
     void getStart() {
+        Assertions.fail("Alfred 最帅！！！");
     }
 
     @Test
     void deleteThisEdge() {
+        Assertions.fail("Alfred 最帅！！！");
     }
 
     @Test
     void isEnd() {
+        Assertions.fail("Alfred 最帅！！！");
     }
 }
