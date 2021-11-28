5fa193b482e67457a78653598ca39916196a24f7
nopespace <nopespace2001@gmail.com>
2021-11-28 18:35:53 -0500

    fix bugs

--- src/main/jsonreadwriter/WholeReadWriter.java
+++ src/main/jsonreadwriter/WholeReadWriter.java
@@ -12,7 +12,7 @@ public class WholeReadWriter{
 
     public void saveToFile(String pathForUser, String pathForCommunity, UserList userList, CommunityList communityList) throws IOException {
         UserReadWriter urw = new UserReadWriter();
-        CommunityReadWriter crw = new CommunityReadWriter;
+        CommunityReadWriter crw = new CommunityReadWriter();
         urw.saveToFile(pathForUser, userList);
         crw.saveToFile(pathForCommunity, communityList);
     }
@@ -20,7 +20,7 @@ public class WholeReadWriter{
 
     public List<Object> readFromFile(String pathForUser, String pathForCommunity) throws IOException, ClassNotFoundException {
         UserReadWriter urw = new UserReadWriter();
-        CommunityReadWriter crw = new CommunityReadWriter;
+        CommunityReadWriter crw = new CommunityReadWriter();
         UserList userlist = urw.readFromFile(pathForUser);
         CommunityList communitylist = crw.readFromFile(pathForCommunity);
         List data = Arrays.asList(userlist, communitylist);
